0f3d0a1efb3edfde858aa4101fb8bb6b4ace5143
HyoTaek-Jang <hyotaek9812@gmail.com>
2024-05-13 21:40:29 +0900

    init: add init setting

---
+++ src/main/kotlin/com/fork/forkfork/ForkforkApplication.kt
@@ -0,0 +1,11 @@
+package com.fork.forkfork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForkforkApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForkforkApplication>(*args)
+}
---
+++ src/test/kotlin/com/fork/forkfork/ForkforkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fork.forkfork
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForkforkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
