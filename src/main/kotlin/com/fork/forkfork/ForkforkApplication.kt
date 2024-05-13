02733b8db507efdbcbee1196f6bbd7d6473a635e
HyoTaek-Jang <hyotaek9812@gmail.com>
2024-05-13 23:36:30 +0900

    [FORKFORK-1] fix: fix swagger server

--- src/main/kotlin/com/fork/forkfork/ForkforkApplication.kt
+++ src/main/kotlin/com/fork/forkfork/ForkforkApplication.kt
@@ -1,11 +1,14 @@
 package com.fork.forkfork
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = [Server(url = "/", description = "Default Server URL")])
 class ForkforkApplication
 
 fun main(args: Array<String>) {
-	runApplication<ForkforkApplication>(*args)
+    runApplication<ForkforkApplication>(*args)
 }
