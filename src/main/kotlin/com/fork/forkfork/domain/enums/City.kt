a3b33d69fe8d7ef111ebe955159111ab7090f279
HyoTaek-Jang <hyotaek9812@gmail.com>
2024-06-16 23:24:30 +0900

    [FORKFORK-12] feat: add city, region

---
+++ src/main/kotlin/com/fork/forkfork/domain/enums/City.kt
@@ -0,0 +1,21 @@
+package com.fork.forkfork.domain.enums
+
+enum class City(val cityName: String) {
+    SEOUL("서울"),
+    GYEONGGI("경기"),
+    INCHEON("인천"),
+    DAEJEON("대전"),
+    DAEGU("대구"),
+    BUSAN("부산"),
+    ULSAN("울산"),
+    GWANGJU("광주"),
+    GANGWON("강원"),
+    SEJONG("세종"),
+    CHUNGCHEONGNAM("충남"),
+    CHUNGCHEONGBUK("충북"),
+    GYEONGSANGNAM("경남"),
+    GYEONGSANGBUK("경북"),
+    JEOLANAM("전남"),
+    JEOLABUK("전북"),
+    JEJU("제주"),
+}
