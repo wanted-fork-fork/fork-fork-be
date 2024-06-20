d2fbec93e2a47bcc4eac7f41738c7830a2bda959
HyoTaek-Jang <hyotaek9812@gmail.com>
2024-06-20 19:29:39 +0900

    [FORKFORK-14] add Info entity

---
+++ src/main/kotlin/com/fork/forkfork/info/domain/IdealPartner.kt
@@ -0,0 +1,3 @@
+package com.fork.forkfork.info.domain
+
+class IdealPartner
---
+++ src/main/kotlin/com/fork/forkfork/info/domain/entity/Info.kt
@@ -0,0 +1,16 @@
+package com.fork.forkfork.info.domain.entity
+
+import com.fork.forkfork.domain.BaseAuditor
+import com.fork.forkfork.info.domain.IdealPartner
+import com.fork.forkfork.info.domain.UserInfo
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document("info")
+class Info(
+    @Id val id: String,
+    val matchMakerId: String,
+    val authorId: String?,
+    val userInfo: UserInfo,
+    val idealPartner: IdealPartner,
+) : BaseAuditor()
