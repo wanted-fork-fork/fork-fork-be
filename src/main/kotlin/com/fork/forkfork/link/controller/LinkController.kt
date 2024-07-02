2941458b01eee7bdf022340c44e18ad053269779
HyoTaek-Jang <hyotaek9812@gmail.com>
2024-07-03 00:40:36 +0900

    [FORKFORK-20] add validate link api

--- src/main/kotlin/com/fork/forkfork/link/controller/LinkController.kt
+++ src/main/kotlin/com/fork/forkfork/link/controller/LinkController.kt
@@ -1,7 +1,10 @@
 package com.fork.forkfork.link.controller
 
+import com.fork.forkfork.link.dto.response.ValidateLinkResponse
 import com.fork.forkfork.link.service.LinkService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController
 class LinkController(val linkService: LinkService) {
     @PostMapping
     fun createLink() = ResponseEntity.ok().body(linkService.createLink())
+
+    @GetMapping("/valid/{linkId}")
+    fun validateLink(
+        @PathVariable linkId: String,
+    ) = ResponseEntity.ok().body(ValidateLinkResponse(linkService.isValidLink(linkId), linkId))
 }
